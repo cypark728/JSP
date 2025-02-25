1f414adb9f4a52ea7486d6f6b721ee2653fc8354
cypark728 <cypark448@gmail.com>
2025-02-25 17:52:12 +0900

    2025-02-25

--- JSPBasic/src/main/java/com/example/controller/MyController.java
+++ JSPBasic/src/main/java/com/example/controller/MyController.java
@@ -7,16 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//1. 유알엘맵핑을 확장자패턴으로 변경
 //@WebServlet("/MyController")
 @WebServlet("*.example") //앞에 뭐가오던 상관없이 .example이라고 끝나면 요청을 맵핑
 public class MyController extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-       
+  
     public MyController() {
         super();
+       
     }
 
-    //get, post요청을 acitons으로 하나로 묶음
+    //2. get, post요청을 action으로 하나로 묶음
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doAction(request, response);
 	}
@@ -24,26 +28,31 @@ public class MyController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doAction(request, response);
 	}
-	
+
 	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//3. 요청을 분리
+		
+		//3. 요청을 분기
 		request.setCharacterEncoding("utf-8");
 		
 		String uri = request.getRequestURI();
 		String contextPath = request.getContextPath();
-		String command = uri.substring(contextPath.length());
+		String command = uri.substring( contextPath.length() );
 		
-//		System.out.println(uri);
-//		System.out.println(contextPath);
+		//System.out.println(uri);
+		//System.out.println(contextPath);
 		System.out.println(command);
 		
-		if(command.equals("/controller/join.example")) {
-			System.out.println("회원가입 작업...");
-		}else if(command.equals("/controller/login.example")){
+		if(command.equals("/controller/join.example") ) {
+			System.out.println("회원가입 작업..");
+		} else if(command.equals("/controller/login.example") ) {
 			System.out.println("로그인 작업..");
-		}else if(command.equals("/controller/getInfo.example")) {
+		} else if(command.equals("/controller/getInfo.example") ) {
 			System.out.println("회원정보 작업..");
 		}
+		
+		
 	}
-
+	
+	
+	
 }
---
+++ MyWeb/src/main/java/com/myweb/controller/UsersController.java
@@ -0,0 +1,65 @@
+package com.myweb.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.myweb.users.service.UsersService;
+import com.myweb.users.service.UsersServiceImpl;
+
+
+@WebServlet("*.users")
+public class UsersController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public UsersController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request,response);
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//한글 처리
+		request.setCharacterEncoding("utf-8");
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String command = uri.substring(conPath.length());
+		
+		System.out.println("요청경로: " + command);
+		
+		//사용할 서비스 선언
+		UsersService service = new UsersServiceImpl();
+		
+		if(command.equals("/users/join.users")) {
+			
+			//mvc2 기본 이동 방식은 포워드 방식입니다.
+			request.getRequestDispatcher("join.jsp").forward(request, response);
+			
+		} else if(command.equals("/users/joinForm.users")) { //회원가입 기능
+			
+			service.join(request, response);
+			
+		} else if(command.equals("/users/login.users")) { //로그인 화면 이동
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		} else if(command.equals("/users/loginForm.users")) {
+			service.login(request, response);
+		} else if(command.equals("/users/mypage.users")) {
+			request.getRequestDispatcher("mypage.jsp").forward(request, response);
+		} else if(command.equals("/users/modifyForm.users")) {
+			service.modify(request, response);
+		}
+	
+	}
+	
+}
